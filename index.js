577c67fe477a0e324027c8f2a0f21e56294d5dd2
billal-fahmid <billalcooom@gmail.com>
2023-05-18 16:59:38 +0600

    server site get data from client site

--- index.js
+++ index.js
@@ -32,6 +32,15 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const toysCollections = client.db('toysDB').collection('allToys')
+
+        app.post('/addToy' , async (req , res) =>{
+            const toy = req.body;
+            console.log(toy)
+        })
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
